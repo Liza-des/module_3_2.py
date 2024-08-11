8bbe526eb1f6b6cd794476c2e18e7b53a8cdb860
Liza-des <lizades@bk.ru>
2024-08-11 18:58:10 +0300

    module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,15 +1,15 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
     endswith = (".com", ".ru", ".net")
     if "@" not in recipient or "@" not in sender:
-        if i not in endswith:
-            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+        for i in [recipient, sender]:
+            if i not in i.endswith(endswith):
+                print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
     elif sender == recipient:
-        print("Нельзя отправить письмо самому себе!")
+         print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
+         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
